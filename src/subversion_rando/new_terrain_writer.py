70ffb850bd99e2018f9cbfa56c0772447d517eaa
beauxq <beauxq@yahoo.com>
2023-09-20 22:16:19 -0700

    TerrainWriter uses space freed by old terrain

--- src/subversion_rando/new_terrain_writer.py
+++ src/subversion_rando/new_terrain_writer.py
@@ -1,25 +1,48 @@
+from bisect import insort_right
 
 from .romWriter import RomWriter, RomWriterType
+from .terrain_patch import Patch, Space
 
 
 class TerrainWriter:
     empty_space: int
     rom_writer: RomWriter
+    emptied: list[Space]
 
     def __init__(self, rom_writer: RomWriter) -> None:
         self.empty_space = 0x2f51c4
         self.rom_writer = rom_writer
+        self.emptied = []
 
-    def write(self, data: bytes, indexes_to_point_to_new_data: list[int]) -> None:
+    def _add_space(self, freed_space: Space) -> None:
+        insort_right(self.emptied, freed_space)
+
+    def _take_space(self, data: bytes) -> int:
+        size_needed = len(data)
+        i = 0
+        while i < len(self.emptied):
+            entry = self.emptied[i]
+            if size_needed < entry.size:
+                self.emptied.pop(i)
+                print(f"using freed rom space {entry}")
+                return entry.location
+            i += 1
+
         # verify the space is empty
         if self.rom_writer.romWriterType == RomWriterType.file:
             for i in range(len(data)):
                 assert self.rom_writer.rom_data[self.empty_space + i] == 0xff
-
-        self.rom_writer.writeBytes(self.empty_space, data)
-        p_level_data = RomWriter.index_to_snes_addr(self.empty_space).to_bytes(3, "little")
-        # print(f"{p_level_data=}")
-        for pointer in indexes_to_point_to_new_data:
-            self.rom_writer.writeBytes(pointer, p_level_data)
-
+        tr = self.empty_space
         self.empty_space += len(data) + 1
+        return tr
+
+    def write(self, patch: Patch) -> None:
+        if patch.freed_space:
+            self._add_space(patch.freed_space)
+        destination = self._take_space(patch.data)
+
+        self.rom_writer.writeBytes(destination, patch.data)
+        p_level_data = RomWriter.index_to_snes_addr(destination).to_bytes(3, "little")
+        # print(f"{p_level_data=}")
+        for pointer in patch.pointers:
+            self.rom_writer.writeBytes(pointer, p_level_data)
