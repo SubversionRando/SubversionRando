4a9d15c9a0d279b5d7ea46aaaf4f1c43a25a8ef6
beauxq <beauxq@yahoo.com>
2024-05-06 11:45:05 -0700

    use https

--- src/subversion_rando/subversion_patch.py
+++ src/subversion_rando/subversion_patch.py
@@ -11,14 +11,14 @@ _FILE_NAME = "subversion.1.2.ips"
 _PATCH_SHA256 = "0c0e0b2f8d034d44f7e2a3d7aace9f35801344a095c0b256453902d43a5a9f6f"
 
 
-def _http_fetch() -> Union[bytes, None]:
+def _https_fetch() -> Union[bytes, None]:
     try:
-        with urlopen("http://edit-sm.art/subversion/patches/subversion.1.2.ips") as response:
+        with urlopen("https://edit-sm.art/subversion/patches/subversion.1.2.ips") as response:
             if response.getcode() == 200:
                 data = response.read()
                 if sha256(data).hexdigest() == _PATCH_SHA256:
                     return data
-                logging.warning("WARNING: http://edit-sm.art/subversion has been tampered with")
+                logging.warning("WARNING: https://edit-sm.art/subversion has been tampered with")
             return None
     except Exception:
         return None
@@ -65,7 +65,7 @@ def get(cache_directory: Union[str, Path] = ".") -> Union[bytes, None]:
     if data:
         _mem_cache = data
         return data
-    data = _http_fetch()
+    data = _https_fetch()
     if data:
         _mem_cache = data
         _fs_store(data, cache_directory)
