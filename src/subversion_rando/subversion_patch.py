3cf6072c4ee664521b26728a70d30c3bf8369ed7
beauxq <beauxq@yahoo.com>
2024-07-01 11:15:33 -0700

    use http (with SHA256) to get patch
    because the AP AppImage was getting an error with https

--- src/subversion_rando/subversion_patch.py
+++ src/subversion_rando/subversion_patch.py
@@ -11,16 +11,26 @@ _FILE_NAME = "subversion.1.2.ips"
 _PATCH_SHA256 = "0c0e0b2f8d034d44f7e2a3d7aace9f35801344a095c0b256453902d43a5a9f6f"
 
 
-def _https_fetch() -> Union[bytes, None]:
+def _http_fetch() -> Union[bytes, None]:
     try:
-        with urlopen("https://edit-sm.art/subversion/patches/subversion.1.2.ips") as response:
+        # https doesn't work with AP AppImage
+
+        # URLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED]
+        # certificate verify failed: unable to get local issuer certificate (_ssl.c:1006)'))
+
+        # I think http is safe because we have the SHA256
+
+        with urlopen("http://edit-sm.art/subversion/patches/subversion.1.2.ips") as response:
             if response.getcode() == 200:
                 data = response.read()
                 if sha256(data).hexdigest() == _PATCH_SHA256:
+                    logging.info("subversion patch download successful")
                     return data
-                logging.warning("WARNING: https://edit-sm.art/subversion has been tampered with")
+                logging.warning("WARNING: http://edit-sm.art/subversion has been tampered with")
+            logging.warning(f"subversion patch download {response.getcode()=}")
             return None
-    except Exception:
+    except Exception as e:
+        logging.warning(f"subversion patch download error {e!r}")
         return None
 
 
@@ -65,7 +75,7 @@ def get(cache_directory: Union[str, Path] = ".") -> Union[bytes, None]:
     if data:
         _mem_cache = data
         return data
-    data = _https_fetch()
+    data = _http_fetch()
     if data:
         _mem_cache = data
         _fs_store(data, cache_directory)
