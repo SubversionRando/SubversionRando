e00fac6e1f290c9b09ce67774164c6fae03ad772
beauxq <beauxq@yahoo.com>
2024-08-10 14:57:40 -0700

    add `TrackerLogic` class

---
+++ tests/test_loadout.py
@@ -0,0 +1,19 @@
+from subversion_rando.area_rando_types import DoorPairs
+from subversion_rando.game import Game, GameOptions
+from subversion_rando.item_data import Items
+from subversion_rando.loadout import Loadout
+from subversion_rando.location_data import new_locations
+
+
+def test_loadout_from_dict_of_counts() -> None:
+    """ make sure `Loadout` ctor works with `Counter` """
+    items = {
+        Items.Energy: 3
+    }
+
+    loadout = Loadout(
+        Game(GameOptions(frozenset(), False, "D", False), new_locations(), DoorPairs(()), 0),
+        items
+    )
+
+    assert loadout.count(Items.Energy) == 3, f"{loadout.count(Items.Energy)=}"
